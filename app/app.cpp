a6a88bdd23afa2d5f4bfd430e7f994002fecd62e
Matko <tkoma999@gmail.com>
2022-10-11 17:25:24 +0200

    Modification of f1.

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,20 @@
 #include "app.h"
-
 #include <format>
+#include <cmath>
 
 namespace vsite::oop::p1 {
+    
     double f1(int a,int b){
-        return a**b;
+
+        double result=pow(a,b);
+        return result;
     }
+    
+
+
+    
+    
+
 
 /*
 * function implementations
