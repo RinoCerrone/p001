8b05d17cd810b63432054a34fcd6292568c4da0c
Matko <tkoma999@gmail.com>
2022-10-12 23:59:54 +0200

    Modfications

--- app/app.cpp
+++ app/app.cpp
@@ -1,33 +1,20 @@
 #include "app.h"
+
 #include <cmath>
 
 namespace vsite::oop::p1 {
     
     double f1(int a,int b){
 
-        double result=pow(a,b);
-        return result;
+        
+        return pow(a,b);
     }
-    bool f2(unsigned long long x,unsigned long long y){
-        if((x<y)==true)return false;
-        else if((x<y)==false)return true;
+    bool f2(){
+        
          
      }
-    int f3(int a,int b,int c,int d){
-        return 20;
-    }
-    int f3(int a,int b,int c){
-        return 22;
-    }
-    int f3(int a,int b){
-        return 22;
-    }
-    int f3(int a){
-        return 29;
-    }
-    int f3(){
-        return 10;
-    }
+    int f3(){}
+  
 
 
     
--- app/app.h
+++ app/app.h
@@ -5,6 +5,18 @@
 
 namespace vsite::oop::p1 
 {
+    using namespace::std;
+     double f1(int a,int b){  
+    }
+    bool f2(bool b){ 
+     }
+    int f3(int arr[],int size){
+
+    }
+    string f4(){
+        
+    }
+    
 /*  
 * function declarations
 */
